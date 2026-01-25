6224591bd182f2cc00a5f7f11a660b13b9d75729
Leung_Wsl <tonyleungwuhan@gmail.com>
2026-01-25 21:20:39 +0800

    Auto Push

---
+++ auto.zsh
@@ -0,0 +1,6 @@
+#! /usr/bin/env zsh
+
+	local commit_msg="${1:-Auto Push}" 
+	git add . && git diff --quiet --cached || git commit -m "$commit_msg" && git push origin "$(git branch --show-current)" 2> /dev/null || git push -u origin "$(git branch --show-current)"
+}
+autoGit $@
