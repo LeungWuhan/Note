6ef47cd67578778865ff0b82fc6569707e967bb1
Leung_Wsl <tonyleungwuhan@gmail.com>
2026-02-18 16:00:01 +0800

    Auto Push (Weekly)

--- CsNote/zshguide/projet/2.zsh
+++ CsNote/zshguide/projet/2.zsh
@@ -2,21 +2,29 @@
 
 # String lookup
 str=abcdef
+echo $str
 
+echo "------------String lookup----------------"
+# "I" is from right to left lookup, if can't find Return 0. 
 echo $str[(I)cd]
 
 echo $str[(I)cdd]
-# "I" is from right to left lookup, if can't find Return 0. 
 # "i" is from left to right, if can't find  return array_size + 1 
- 
+#
 echo $str[(i)cd]
 
 #forloop String
+
+echo "------------forloop String----------------"
 for i ({1..$#str}) {
-  echo $str[i]
+  echo "$str[i] \n"
 }
 
 #echo {0..100} 
 
-echo ${str/bc/ef}
-echo ${str//ab}
+# Replace string character
+echo "-------------Replace string character---------------"
+
+echo "${str/ab} "
+
+
